460d4787c85a9dd3de6dada7093f35d7eedecf95
darmads <50747882+darmads@users.noreply.github.com>
2019-08-31 19:11:05 +0700

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,5 +1,29 @@
 function getvalue(){
     var search = document.getElementById("search").value;
+    fetch('https://swapi.co/api/people')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        const { results } = data;
+    for(var i=0; i<results.length; i++){
+        const { name, height } = results[i]
+        if(name==search){
+            document.querySelector("tbody").textContent = null;
+            var tr = createNode('tr')
+            tr.setAttribute('id', i)
+            tbody.appendChild(tr)
+            for(var y=0; y<2; y++){
+                var abc = getId(i)
+                const key = [name, height]
+                var td = createNode('td')
+                var textnode = document.createTextNode(key[y]);
+                td.appendChild(textnode);
+                abc.appendChild(td)
+            }
+        }
+    }
+    })
 }
 const getId = (id) => {
     return document.getElementById(id);
